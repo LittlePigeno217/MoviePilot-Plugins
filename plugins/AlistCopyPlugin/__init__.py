d391b6cbce5409fa12586d09c41279efabdfce15
LittlePigeno <LittlePigeno217@gmail.com>
2025-10-28 02:37:52 +0800

    Update __init__.py

--- plugins/AlistCopyPlugin/__init__.py
+++ plugins/AlistCopyPlugin/__init__.py
@@ -20,7 +20,7 @@ class AlistCopyPlugin(_PluginBase):
     plugin_name = "OpenList自动复制"
     plugin_desc = "实现OpenList多目录间文件复制自动化"
     plugin_icon = "Alist_B.png"
-    plugin_version = "1.0"
+    plugin_version = "1.0.0"
     plugin_author = "LittlePigeno"
     author_url = "https://github.com/LittlePigeno217"
     plugin_config_prefix = "alistcopy_"
