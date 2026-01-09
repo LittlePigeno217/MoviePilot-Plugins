1a20649eafa5e3de406d0b9df862266e65ab4a4e
LittlePigeno <LittlePigeno217@gmail.com>
2026-01-10 00:36:37 +0800

    删除vue版本

--- plugins/openlistmanagervue/frontend/src/components/fix_page.py
+++
@@ -1,7 +0,0 @@
-﻿import re
-with open('Page.vue', 'r', encoding='utf-8') as f:
-    content = f.read()
-content = content.replace('props.api.get(\\plugin/\/status)', "props.api.get(\plugin/\/status\)")
-content = content.replace('props.api.post(\\plugin/\/run)', "props.api.post(\plugin/\/run\)")
-with open('Page.vue', 'w', encoding='utf-8') as f:
-    f.write(content)
