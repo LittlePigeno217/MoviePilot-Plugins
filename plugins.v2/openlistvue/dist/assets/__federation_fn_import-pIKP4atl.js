6b25a5ae50c90b1407f0f7ca5b66094c12d2ef2a
LittlePigeno <LittlePigeno217@gmail.com>
2026-01-20 01:54:00 +0800

    feat: 添加缓存监控插件并移除OpenListVue插件
    - 新增缓存监控插件，实现实时监控和管理系统缓存数据
    - 移除不再使用的OpenListVue插件及其相关文件
    - 更新package.json文件，添加cachemonitor插件配置

--- plugins.v2/openlistvue/dist/assets/__federation_fn_import-pIKP4atl.js
+++
@@ -1,3 +0,0 @@
-import { n as getSharedFromRuntime, r as importShared, t as getSharedFromLocal } from "./_virtual___federation_fn_import-j-cuoq0S.js";
-import "./preload-helper-GxVotskq.js";
-export { importShared, getSharedFromLocal as importSharedLocal, getSharedFromRuntime as importSharedRuntime };
